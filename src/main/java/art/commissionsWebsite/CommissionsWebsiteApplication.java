784b14174c8044217bfb309e4359888036e3a012
Markelle Ngishu <mngish1@wgu.edu>
2024-10-11 23:26:13 -0500

    Initial Commit

---
+++ src/main/java/art/commissionsWebsite/CommissionsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package art.commissionsWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommissionsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommissionsWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/art/commissionsWebsite/CommissionsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package art.commissionsWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommissionsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
